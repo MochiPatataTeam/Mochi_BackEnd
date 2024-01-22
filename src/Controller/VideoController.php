1f3a9159f8001a8526dde4745ae64987105b810b
rksl64 <iferreirafernandez@safareyes.es>
2024-01-22 19:10:23 +0100

    Ismael - Subo CRUD de video completo

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -11,6 +11,7 @@ use App\Entity\Video;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Usuario;
 
 #[Route('/api/video')]
 class VideoController extends AbstractController
@@ -34,20 +35,58 @@ class VideoController extends AbstractController
         }
         return $this->json($listaVideosDTO, Response::HTTP_OK);
     }
+
     #[Route('', name: 'crear_video', methods: ['POST'])]
     public function crearvideo (EntityManagerInterface $entityManager, Request $request): JsonResponse
     {
         $data =json_decode($request->getContent(),true);
-        $video = new Video();
-        $video->setTitulo($data['titulo']);
-        $video->setDescripcion($data['descripcion']);
-        $video->setUrl($data['url']);
-        //$video->setCanal($data['']); //ESTO FALLA, HAY QUE PREGUNTAR
 
-        $entityManager->persist($video);
+        $nuevoVideo = new Video();
+        $nuevoVideo->setTitulo($data['titulo']);
+        $nuevoVideo->setDescripcion($data['descripcion']);
+        $nuevoVideo->setUrl($data['url']);
+
+        $usuario = $entityManager->getRepository(Usuario::class)->findBy(["id" => $data['canal']]);
+        $nuevoVideo->setCanal($usuario[0]);
+
+        $entityManager->persist($nuevoVideo);
         $entityManager->flush();
 
         return $this->json(['message' => 'Video creado'], Response::HTTP_CREATED);
     }
 
+    #[Route('/{id}', name: "editar_video", methods: ["PUT"])]
+    public function editarvideo (EntityManagerInterface $entityManager, Request $request, $id):JsonResponse
+    {
+        $data = json_decode($request-> getContent(), true);
+
+        $video = $entityManager->getRepository(Video::class)->find($id);
+        if (!$video) {
+            return $this->json(['message' => 'Video no encontrado'], Response::HTTP_NOT_FOUND);
+        }
+
+        $video->setTitulo($data['titulo']);
+        $video->setDescripcion($data['descripcion']);
+        $video->setUrl($data['url']);
+
+        $usuario = $entityManager->getRepository(Usuario::class)->findBy(["id" => $data['canal']]);
+        $video->setCanal($usuario[0]);
+
+        $entityManager->flush();
+
+        return $this->json(['message' => 'Video modificado'], Response::HTTP_OK);
+
+    }
+    #[Route('/{id}', name: "delete_by_id", methods: ["DELETE"])]
+    public function deleteById(EntityManagerInterface $entityManager, $id):JsonResponse
+    {
+
+        $video = $entityManager->getRepository(Video::class)->find($id);
+
+        $entityManager->remove($video);
+        $entityManager->flush();
+
+        return $this->json(['message' => 'Video eliminado'], Response::HTTP_OK);
+
+    }
 }

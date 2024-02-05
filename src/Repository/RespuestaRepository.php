fb98a3fc243830cd169a01294b82e03de233971c
rksl64 <iferreirafernandez@safareyes.es>
2024-02-05 18:28:14 +0100

    Ismael - subo metodos y cambio de querys

--- src/Repository/RespuestaRepository.php
+++ src/Repository/RespuestaRepository.php
@@ -21,15 +21,18 @@ class RespuestaRepository extends ServiceEntityRepository
         parent::__construct($registry, Respuesta::class);
     }
 
-    public function respuestavideoID(int $id)
+    public function respuestavideoID(int $id, int $id_comentario)
     {
-        return $this->createQueryBuilder('r')
-            ->select('c.id as comentario_id', 'c.fav', 'c.dislike','c.comentario', 'u.username as username_respuesta','r.id' ,'r.mensaje')
-            ->join('r.comentario', 'c')
-            ->join('r.usuario', 'u')
-            ->where('c.id = :id_comentario')
-            ->setParameter('id_comentario', $id)
-            ->getQuery()
-            ->getResult();
+        $entityManager = $this->getEntityManager();
+        $sql = 'select c.id, c.fav, c.dislike, u.username, c.comentario, r.id as respuesta_id, ru.username as respuesta_username, r.mensaje  from mochi.comentario c 
+                join mochi.video v on v.id = c.id_video 
+                join mochi.usuario u on c.id_usuario = u.id
+                join mochi.respuesta r on r.id_comentario = c.id
+                join mochi.usuario ru on ru.id = r.id_usuario 
+                where v.id=:id and r.id_comentario = :comentario';
+
+        $query = $entityManager->getConnection()->executeQuery($sql,['id'=>$id,'comentario'=>$id_comentario],['id'=>\PDO::PARAM_INT,'comentario'=>\PDO::PARAM_INT,]);
+        $result = $query->fetchAllAssociative();
+        return $result;
     }
 }
